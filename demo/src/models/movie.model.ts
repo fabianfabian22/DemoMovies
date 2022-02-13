58280d329f656cd7f3500808a6d627a9b2f99bc4
fabianfabian22 <fabianfabian22@hotmail.com>
2022-02-13 13:19:45 +0100

    instalamos librerias de Ngrx y creamos el modelo de movie

---
+++ demo/src/models/movie.model.ts
@@ -0,0 +1,11 @@
+export interface Movie {
+  id: string,
+  title:  string,
+  poster: string,
+  genre: string [],
+  year: number,
+  duration: number,
+  imdbRating: number,
+  actors:number []
+
+}
